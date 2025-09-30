cc28699a620f246e8e18a9fa482741b744861a70
Ronnie Jang <ronniej@naver.com>
2025-09-30 23:16:43 +0900

    isInvulnerable 변수 이름 오타 수정

--- Assets/RPGGame/Scripts/Damageable.cs
+++ Assets/RPGGame/Scripts/Damageable.cs
@@ -10,7 +10,7 @@ namespace RPGGame
         [SerializeField] private UnityEvent<float> OnDamageReceived;
 
         // 대미지를 받았을 때 중복으로 대미지를 계속해서 받지 않도록 하는 변수(무적 모드)
-        [SerializeField] private bool isInVulnerable = false;
+        [SerializeField] private bool isInvulnerable = false;
 
         // 대미지를 중복으로 입지 않도록 유지하는 시간(단위: 초).
         [SerializeField] private float invulnerableTime = 0.2f;
@@ -24,10 +24,10 @@ namespace RPGGame
         private void Update()
         {
             // 대미지를 입지 않는 무적 모드일 때 무적 모드 해제까지의 시간 계산
-            if (isInVulnerable && Time.time > time + invulnerableTime)
+            if (isInvulnerable && Time.time > time + invulnerableTime)
             {
                 // 무적 모드 해제.
-                isInVulnerable = false;
+                isInvulnerable = false;
             }
         }
 
@@ -42,13 +42,13 @@ namespace RPGGame
         public void ReceiveDamage(float damageAmount)
         {
             // 무적 상태라면 대미지를 처리하지 않고 함수 종료
-            if (isInVulnerable)
+            if (isInvulnerable)
             {
                 return;
             }
 
             // 무적 상태 On
-            isInVulnerable = true;
+            isInvulnerable = true;
 
             // 무적 상태 시작 시간 저장
             time = Time.time;
